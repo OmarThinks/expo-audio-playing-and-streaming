9acf922fe53a0f3a2b347271219446585a9acdcc
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-08-28 11:07:46 +0300

    feat: separeted useBase64PcmAudioPlayer hook

--- docs/thinking.md
+++ docs/thinking.md
@@ -5,8 +5,5 @@
 - remove the native module ✅
 - Adjust and recorder tabs ✅
 - Create 3 new empty tabs, to learn on them, let them be in the last, call them 1,2, and 3 ✅
-- Create a new hook to learn on it in learning 1
-- Document how audio playing hook works. And understand it.
-
-- Document how the Audio recording hook works. And understand it.
-- Document and understand how the Audio enqueueing works, and understand it.
+- Create a new hook to learn on it in learning 1 ✅
+- clean the hooks folder from expo hooks
--- expo-audio-playing-and-streaming/app/(tabs)/Learn1.tsx
+++ expo-audio-playing-and-streaming/app/(tabs)/Learn1.tsx
@@ -1,70 +1,9 @@
+import { useBase64PcmAudioPlayer } from "@/audioHooks/useBase64PcmAudioPlayer";
 import { dummyBase64Text } from "@/samples/dummyBase64Text";
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React from "react";
 import { Button, Text, View } from "react-native";
-import { AudioBufferSourceNode, AudioContext } from "react-native-audio-api";
-
-const useBase64PcmAudioPlayer = ({ sampleRate }: { sampleRate: number }) => {
-  const audioContextRef = useRef<AudioContext | null>(null);
-  const audioBufferSourceNodeRef = useRef<AudioBufferSourceNode | null>(null);
-  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
-
-  const cleanUp = useCallback(() => {
-    setIsAudioPlaying(false);
-
-    try {
-      audioBufferSourceNodeRef.current?.stop?.();
-    } catch {}
-    audioBufferSourceNodeRef.current = null;
-  }, []);
-
-  useEffect(() => {
-    cleanUp();
-
-    const audioContext = new AudioContext({ sampleRate });
-    audioContextRef.current = audioContext;
-
-    return () => {
-      cleanUp();
-    };
-  }, [cleanUp, sampleRate]);
-
-  const playPcmBase64Audio = useCallback(
-    async ({ base64String }: { base64String: string }) => {
-      if (audioContextRef.current) {
-        const audioBuffer =
-          await audioContextRef.current?.decodePCMInBase64Data(base64String);
-
-        const audioBufferSourceNode =
-          audioContextRef.current.createBufferSource();
-        audioBufferSourceNode.connect(audioContextRef.current.destination);
-
-        audioBufferSourceNode.buffer = audioBuffer;
-        setIsAudioPlaying(true);
-        audioBufferSourceNode.onEnded = () => {
-          cleanUp();
-        };
-        audioBufferSourceNode.start();
-        audioBufferSourceNodeRef.current = audioBufferSourceNode;
-      }
-    },
-    [cleanUp]
-  );
-
-  return { isAudioPlaying, playPcmBase64Audio, stopPlayingAudio: cleanUp };
-};
 
 const Learn1 = () => {
-  /*const myFunction = useCallback(async () => {
-    const audioContext = new AudioContext({ sampleRate: 16000 });
-    const audioBuffer = await audioContext.decodePCMInBase64Data(
-      dummyBase64Text
-    );
-    const playerNode = audioContext.createBufferSource();
-    playerNode.connect(audioContext.destination);
-    playerNode.buffer = audioBuffer;
-    playerNode.start();
-  }, []);*/
-
   const { isAudioPlaying, playPcmBase64Audio, stopPlayingAudio } =
     useBase64PcmAudioPlayer({ sampleRate: 16000 });
 
