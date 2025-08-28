11e9a2654d002cf9f476b82c0af84e9ad9199d8f
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-08-28 09:03:35 +0300

    chore: deleted modules folder

--- expo-audio-playing-and-streaming/modules/audio-streamer/index.ts
+++
@@ -1,6 +0,0 @@
-// Reexport the native module. On web, it will be resolved to AudioStreamerModule.web.ts
-// and on native platforms to AudioStreamerModule.ts
-export * from "./src/AudioStreamer.types";
-export { default } from "./src/AudioStreamerModule";
-export { default as AudioStreamerView } from "./src/AudioStreamerView";
-export { useAudioStream } from "./src/useAudioStream";
